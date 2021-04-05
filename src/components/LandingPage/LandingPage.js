175d180315bdb455bc183a0b5174d6890c50802c
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-04-05 17:51:34 +0100

    Eliminated second section of landing page and adjusted height of content

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,28 +5,24 @@ import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
-    <>
-      <div className="landing-head">
-        <header className="cointoss-head">
-          <Link className="headCoinLink" to="/coins">
-            <h1>Coin Toss</h1>
-          </Link>
-        </header>
-        <Nav />
-        <div className="slogan">
-          <section className="slogan">
-            <h2>
-              Trending cryptocurrencies in the palm of your hand. Cast your
-              votes!
-            </h2>
-            <div className="landing-info">
-              <p>This is information for the app. How to use it, etc.</p>
-            </div>
-          </section>
-        </div>
+    <div className="landing-head">
+      <header className="cointoss-head">
+        <Link className="headCoinLink" to="/coins">
+          <h1>Coin Toss</h1>
+        </Link>
+      </header>
+      <Nav />
+      <div className="slogan">
+        <section className="slogan">
+          <h2>
+            Trending cryptocurrencies in the palm of your hand. Cast your votes!
+          </h2>
+          <div className="landing-info">
+            <p>This is information for the app. How to use it, etc.</p>
+          </div>
+        </section>
       </div>
-      <section className="info">Second Section</section>
-    </>
+    </div>
   );
 }
 
